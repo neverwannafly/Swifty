9f48251158caa2b7c85a77a5164167e6d1a55118
Shubham Anand <shubham_chess@live.com>
2020-04-05 00:19:16 +0530

    create basic parser

--- swifty/Program.cs
+++ swifty/Program.cs
@@ -8,13 +8,23 @@ namespace swifty {
             while (true) {
                 Console.Write("> ");
                 string line = Console.ReadLine();
-                var parser = new Parser(line);
-                var expression = parser.Parse();
-
+                var syntaxTree = SyntaxTree.Parse(line);
                 var color = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
-                print(expression);
+                print(syntaxTree.Root);
                 Console.ForegroundColor = color;
+
+                if (!syntaxTree.Diagnostics.Any()) {
+                    Evaluator eval = new Evaluator(syntaxTree.Root);
+                    int result = eval.Evaluate();
+                    Console.WriteLine(result);
+                } else {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    foreach(var diagnostic in syntaxTree.Diagnostics) {
+                        Console.WriteLine(diagnostic);
+                    }
+                    Console.ForegroundColor = color;
+                }
             }
         }
         static void print(SyntaxNode node, string indent="", bool isLast=true) {
@@ -46,6 +56,7 @@ namespace swifty {
         BadToken,
         NumberExpression,
         BinaryExpression,
+        ParathesisExpression,
     }
     class SyntaxToken : SyntaxNode {
         public SyntaxToken(SyntaxKind kind, int position, string text, object value) {
@@ -65,9 +76,11 @@ namespace swifty {
     class Lexer {
         private readonly string _text;
         private int _position;
+        private List<string> _diagnostics = new List<string>();
         public Lexer(string text) {
             _text = text;
         }
+        public IEnumerable<string> Diagnostics => _diagnostics;
         private char Current {
             get {
                 if (_position>=_text.Length) return '\0';
@@ -86,7 +99,9 @@ namespace swifty {
                 while (char.IsDigit(Current)) Next();
                 int len = _position - start;
                 string text = _text.Substring(start, len);
-                int.TryParse(text, out var value);
+                if (!int.TryParse(text, out var value)){
+                    _diagnostics.Add($"The number {_text} is invalid Int32");
+                };
                 return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
             }
             if (char.IsWhiteSpace(Current)) {
@@ -103,11 +118,13 @@ namespace swifty {
                 case '/':  return new SyntaxToken(SyntaxKind.DivideToken, _position++, "/", null);
                 case '(':  return new SyntaxToken(SyntaxKind.LeftParanthesisToken, _position++, "(", null);
                 case ')':  return new SyntaxToken(SyntaxKind.RightParanthesisToken, _position++, ")", null);
-                default:  return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position-1, 1), null);
+                default: {  
+                    _diagnostics.Add($"ERROR: Bad character input : '{Current}'");
+                    return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position-1, 1), null);
+                }
             }
         }
     }
-
     abstract class SyntaxNode {
         public abstract SyntaxKind Kind {get;}
         public abstract IEnumerable<SyntaxNode> GetChildren();
@@ -141,9 +158,39 @@ namespace swifty {
             yield return Right;
         }
     }
-
+    sealed class ParanthesisExpressionSyntax: ExpressionSyntax {
+        public ParanthesisExpressionSyntax(SyntaxToken leftParanthesis, ExpressionSyntax expr, SyntaxToken rightParanthesis) {
+            LeftParanthesisToken = leftParanthesis;
+            Expression = expr;
+            RightParanthesisToken = rightParanthesis;
+        }
+        public SyntaxToken LeftParanthesisToken {get;}
+        public ExpressionSyntax Expression {get;}
+        public SyntaxToken RightParanthesisToken {get;}
+        public override SyntaxKind Kind => SyntaxKind.ParathesisExpression;
+        public override IEnumerable<SyntaxNode> GetChildren() {
+            yield return LeftParanthesisToken;
+            yield return Expression;
+            yield return RightParanthesisToken;
+        }
+    }
+    sealed class SyntaxTree {
+        public SyntaxTree(IEnumerable<string> diagnostics, ExpressionSyntax root, SyntaxToken endofFileToken) {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndofFileToken = endofFileToken;
+        }
+        public IReadOnlyList<string> Diagnostics {get;}
+        public ExpressionSyntax Root {get;}
+        public SyntaxToken EndofFileToken {get;}
+        public static SyntaxTree Parse(string text) {
+            var parser = new Parser(text);
+            return parser.Parse();
+        }
+    }
     class Parser {
         private readonly SyntaxToken[] _tokens;
+        private List<string> _diagnostics = new List<string>();
         private int _position;
         public Parser(string text) {
             Lexer lexer = new Lexer(text);
@@ -156,7 +203,9 @@ namespace swifty {
                 }
             } while (token.Kind!=SyntaxKind.EndofFileToken);
             _tokens = tokens.ToArray();
+            _diagnostics.AddRange(lexer.Diagnostics);
         }
+        public IEnumerable<string> Diagnostics => _diagnostics;
         private SyntaxToken Peek(int offset) {
             int index = _position + offset;
             if (index >= _tokens.Length) return _tokens[_tokens.Length-1];
@@ -169,12 +218,31 @@ namespace swifty {
             return current; 
         }
         private SyntaxToken Match(SyntaxKind kind) {
+            // while(Current.Kind == SyntaxKind.WhitespaceToken) { NextToken();}
             if (Current.Kind == kind) return NextToken();
+            _diagnostics.Add($"ERROR: Unexpected token <{Current.Kind}>, expected <{kind}>");
             return new SyntaxToken(kind, Current.Position, null, null);
         }
-        public ExpressionSyntax Parse() {
-            var left = ParsePrimaryExpression();
+        private ExpressionSyntax ParseExpression() {
+            return ParseTerm();
+        }
+        public SyntaxTree Parse() {
+            var expression = ParseTerm();
+            var endofFileToken = Match(SyntaxKind.EndofFileToken);
+            return new SyntaxTree(_diagnostics ,expression, endofFileToken);
+        }
+        private ExpressionSyntax ParseTerm() {
+            var left = ParseFactor();
             while (Current.Kind == SyntaxKind.PlusToken || Current.Kind==SyntaxKind.MinusToken) {
+                var operationToken = NextToken();
+                var right = ParseFactor();
+                left = new BinaryExpressionSyntax(left, operationToken, right);
+            }
+            return left;
+        }
+        private ExpressionSyntax ParseFactor() {
+            var left = ParsePrimaryExpression();
+            while (Current.Kind==SyntaxKind.StarToken || Current.Kind==SyntaxKind.DivideToken) {
                 var operationToken = NextToken();
                 var right = ParsePrimaryExpression();
                 left = new BinaryExpressionSyntax(left, operationToken, right);
@@ -182,8 +250,44 @@ namespace swifty {
             return left;
         }
         private ExpressionSyntax ParsePrimaryExpression() {
+            if (Current.Kind == SyntaxKind.LeftParanthesisToken) {
+                var left = NextToken();
+                var expression = ParseExpression();
+                var right = Match(SyntaxKind.RightParanthesisToken);
+                return new ParanthesisExpressionSyntax(left, expression, right);
+            }
             var numberToken = Match(SyntaxKind.NumberToken);
             return new NumberExpressionSyntax(numberToken);
+        }
+    }
+    class Evaluator {
+        private readonly ExpressionSyntax _root;
+        public Evaluator(ExpressionSyntax root) {
+            _root = root;
+        }
+        public int Evaluate() {
+            return EvaluateExpression(_root);
+        }
+        private int EvaluateExpression(ExpressionSyntax root) {
+            if (root is NumberExpressionSyntax n) {
+                return (int)n.NumberToken.Value;
+            }
+            if (root is BinaryExpressionSyntax b) {
+                var left = EvaluateExpression(b.Left);
+                var right = EvaluateExpression(b.Right);
+
+                switch(b.OperatorToken.Kind) {
+                    case SyntaxKind.PlusToken: return left + right;
+                    case SyntaxKind.MinusToken: return left - right;
+                    case SyntaxKind.StarToken: return left * right;
+                    case SyntaxKind.DivideToken: return left / right;
+                    default: throw new Exception($"Unexpected binary operator {b.OperatorToken.Kind}");
+                }
+            }
+            if (root is ParanthesisExpressionSyntax p) {
+                return EvaluateExpression(p.Expression);
+            }
+            throw new Exception($"Unexpected node {root.Kind}");
         }
     }
 
