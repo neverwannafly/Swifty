1e398aa151267bc5e19c85b2726654b6a54f8098
Shubham Anand <shubham_chess@live.com>
2020-04-05 19:04:12 +0530

    define precedence of boolean operators

--- swifty/Code/Syntax/SyntaxRules.cs
+++ swifty/Code/Syntax/SyntaxRules.cs
@@ -2,18 +2,22 @@ namespace swifty.Code.Syntaxt {
     internal static class SyntaxRules {
         internal static int GetBinaryOperatorPrecendence(this SyntaxKind kind) {
             switch(kind) {
-                case SyntaxKind.PlusToken:      return 1;
-                case SyntaxKind.MinusToken:     return 1;
-                case SyntaxKind.StarToken:      return 2;
-                case SyntaxKind.DivideToken:    return 3;
-                default: return 0;
+                case SyntaxKind.DivideToken:    return 5;
+                case SyntaxKind.StarToken:      return 4;
+                case SyntaxKind.PlusToken:      return 3;
+                case SyntaxKind.MinusToken:     return 3;
+                case SyntaxKind.AndToken:       return 2;
+                case SyntaxKind.OrToken:        return 1;
+                default:                        return 0;
             }
         }
         internal static int GetUnaryOperatorPrecedence(this SyntaxKind kind) {
             switch(kind) {
-                case SyntaxKind.PlusToken:      return 4;
-                case SyntaxKind.MinusToken:     return 4;
-                default: return 0;
+                case SyntaxKind.AndToken:       return 8;
+                case SyntaxKind.OrToken:        return 7;
+                case SyntaxKind.PlusToken:      return 6;
+                case SyntaxKind.MinusToken:     return 6;
+                default:                        return 0;
             }
         }
 
