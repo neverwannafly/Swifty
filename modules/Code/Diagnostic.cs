86600c79b5a996449d05ffdbb9f0822744cab503
Shubham Anand <shubham_chess@live.com>
2020-04-06 19:15:12 +0530

    add Diagnostic class

---
+++ modules/Code/Diagnostic.cs
@@ -0,0 +1,11 @@
+namespace swifty.Code {
+    public sealed class Diagnostic {
+        public Diagnostic(TextSpan span, string message) {
+            Span = span;
+            Message = message;
+        }
+        public TextSpan Span {get;}
+        public string Message {get;}
+        public override string ToString() => Message;
+    }
+}
---
+++ modules/Code/TextSpan.cs
@@ -0,0 +1,11 @@
+namespace swifty.Code {
+    public struct TextSpan {
+        public TextSpan(int start, int length) {
+            Start = start;
+            Length = length;
+        }
+        public int Start {get;}
+        public int Length {get;}
+        public int End => Start + Length;
+    }
+}
