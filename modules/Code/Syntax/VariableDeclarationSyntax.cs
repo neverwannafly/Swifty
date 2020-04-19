ae0f4efd35189e8122a14f8ba89c10c1fdc697b3
Shubham Anand <shubham_chess@live.com>
2020-04-19 08:44:30 +0530

    create prop field IsReadOnly

--- modules/Code/Syntax/VariableDeclarationSyntax.cs
+++ modules/Code/Syntax/VariableDeclarationSyntax.cs
@@ -2,16 +2,18 @@ using System.Collections.Generic;
 
 namespace swifty.Code.Syntaxt {
     public sealed class VariableDeclarationSyntax : StatementSyntax {
-        public VariableDeclarationSyntax(SyntaxToken keyword, SyntaxToken identifier, SyntaxToken assignmentToken, ExpressionSyntax initializer) {
+        public VariableDeclarationSyntax(SyntaxToken keyword, SyntaxToken identifier, SyntaxToken assignmentToken, ExpressionSyntax initializer, bool isReadOnly=false) {
             Keyword = keyword;
             Identifier = identifier;
             AssignmentToken = assignmentToken;
             Initializer = initializer;
+            IsReadOnly = isReadOnly;
         }
         public SyntaxToken Keyword {get;} 
         public SyntaxToken Identifier {get;} 
         public SyntaxToken AssignmentToken {get;}
         public ExpressionSyntax Initializer {get;}
+        public bool IsReadOnly {get;}
         public override SyntaxKind Kind => SyntaxKind.VariableDeclarationStatement;
         public override IEnumerable<SyntaxNode> GetChildren() {
             yield return Keyword;
