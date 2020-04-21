a73319085f1a11d7c6d09d5cfdcd1b934705aee7
Shubham Anand <shubham_chess@live.com>
2020-04-21 11:15:03 +0530

    add configurations for tree and output client

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,16 @@
-let config = {
+let editorConfig = {
     theme: "twilight",
     smartIndent: false,
     lineNumbers: true,
     autofocus: true,
 };
+
+let outputConfig = {
+    theme : "twilight",
+    readOnly: 'nocursor',
+}
+
+let treeConfig = {
+    theme: "twilight",
+    readOnly: "nocursor",
+}
