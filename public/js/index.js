b4bf1ad46eba9df66bda15dada8ead32a2851bb9
Shubham Anand <shubham_chess@live.com>
2020-04-21 11:15:34 +0530

    complete run and build requests

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,21 @@
-const element = document.getElementById("editor");
 const runButton = document.getElementById("run-icon");
 const buildButton = document.getElementById("build-icon");
+const editor = document.getElementById("editor");
+const output = document.getElementById("output")
+const tree = document.getElementById("tree")
 
-const codeMirrorClient = CodeMirror(element, config);
+const editorClient = CodeMirror(editor, editorConfig);
+$("#editor").children()[0].id = "editor-client";
+
+const outputClient = CodeMirror(output, outputConfig);
+$("#output").children()[0].id = "output-client";
+
+const treeClient = CodeMirror(tree, treeConfig);
+$("#tree").children()[0].id = "tree-client";
 
 buildButton.addEventListener('click', function(){
-    const code = codeMirrorClient.getValue();
-    const lines = codeMirrorClient.lineCount();
+    const code = editorClient.getValue();
+    const lines = editorClient.lineCount();
     $.ajax({
         url: '/build',
         data: {
@@ -14,7 +23,15 @@ buildButton.addEventListener('click', function(){
             lines: lines,
         },
         success: function(data) {
-            console.log(data);
+            let text = ``;
+            if (data.result) {
+                text = text + `${data.result[data.result.length-1]}\n`;
+            }
+            if (data.error) {
+                text = text + `${data.error.join("\n")}`;
+            }
+            outputClient.setValue(text);
+            treeClient.setValue('');
         },
         error: function(err) {
             console.log(err);
@@ -23,5 +40,30 @@ buildButton.addEventListener('click', function(){
 });
 
 runButton.addEventListener('click', function(){
-    console.log("Bob the builder! Kar ke dikhaenge!")
+    const code = editorClient.getValue();
+    const lines = editorClient.lineCount();
+    $.ajax({
+        url: '/run',
+        data: {
+            data: code,
+            lines: lines,
+        },
+        success: function(data) {
+            let text = ``;
+            if (data.compilationResult) {
+                text = text + `${data.compilationResult[data.compilationResult.length-1]}\n`;
+            }
+            if (data.error) {
+                text = text + `${data.error.join("\n")}\n`;
+            } else {
+                text = text + `${data.result.join("\n")}\n`;
+            }
+            let tree = `${data.syntaxTree.join("\n")}\n`;
+            outputClient.setValue(text);
+            treeClient.setValue(tree);
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
 });
