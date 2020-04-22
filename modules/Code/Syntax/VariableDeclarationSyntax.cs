f969eb1ee5eb957306cd3650ebb8314d05b911c2
Shubham Anand <shubham_chess@live.com>
2020-04-22 22:11:16 +0530

    include const keyword in variable declaration

--- modules/Code/Syntax/VariableDeclarationSyntax.cs
+++ modules/Code/Syntax/VariableDeclarationSyntax.cs
@@ -2,21 +2,26 @@ using System.Collections.Generic;
 
 namespace swifty.Code.Syntaxt {
     public sealed class VariableDeclarationSyntax : StatementSyntax {
-        public VariableDeclarationSyntax(SyntaxToken keyword, SyntaxToken identifier, SyntaxToken assignmentToken, ExpressionSyntax initializer, bool isReadOnly=false) {
-            Keyword = keyword;
+        public VariableDeclarationSyntax(SyntaxToken constKeyword, SyntaxToken datatypeKeyword, SyntaxToken identifier, SyntaxToken assignmentToken, ExpressionSyntax initializer, bool isReadOnly=false) {
+            ConstKeyword = constKeyword;
+            DatatypeKeyword = datatypeKeyword;
             Identifier = identifier;
             AssignmentToken = assignmentToken;
             Initializer = initializer;
             IsReadOnly = isReadOnly;
         }
-        public SyntaxToken Keyword {get;} 
+        public SyntaxToken ConstKeyword {get;} 
+        public SyntaxToken DatatypeKeyword {get;} 
         public SyntaxToken Identifier {get;} 
         public SyntaxToken AssignmentToken {get;}
         public ExpressionSyntax Initializer {get;}
         public bool IsReadOnly {get;}
         public override SyntaxKind Kind => SyntaxKind.VariableDeclarationStatement;
         public override IEnumerable<SyntaxNode> GetChildren() {
-            yield return Keyword;
+            if (ConstKeyword!=null) {
+                yield return ConstKeyword;
+            }
+            yield return DatatypeKeyword;
             yield return Identifier;
             yield return AssignmentToken;
             yield return Initializer;
